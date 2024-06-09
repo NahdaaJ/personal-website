0708a046460177f004eea212717cf9edd76da34c
Nahdaa Jawed <nahdaajawed@gmail.com>
2024-06-09 02:16:32 +0100

    Website minor restart.

--- scripts/index.js
+++ scripts/index.js
@@ -15,11 +15,12 @@ function loadComponent(containerId, filePath) {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-  loadComponent('section-homepage', 'components/homepage/homepage.html');
-  loadComponent('section-about-me', 'components/about-me/about-me.html');
-  loadComponent('section-experience', 'components/experience/experience.html');
-  loadComponent('section-projects', 'components/projects/projects.html');
-  loadComponent('section-contact', 'components/contact/contact.html');
+  loadComponent('desktop-nav', 'content/nav.html');
+  loadComponent('section-homepage', 'content/homepage.html');
+  // loadComponent('section-about-me', 'content/about-me.html');
+  // loadComponent('section-experience', 'content/experience.html');
+  // loadComponent('section-projects', 'content/projects.html');
+  // loadComponent('section-contact', 'content/contact.html');
 });
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -29,31 +30,31 @@ function tempFunction() {
   alert("You got pranked! (つ≧▽≦)つ")
 }
 
-function customScroll(event) {
-  const currentPhoneNavState = phoneNav.getAttribute("expanded");
-  event.preventDefault();
+// function customScroll(event) {
+//   const currentPhoneNavState = phoneNav.getAttribute("expanded");
+//   event.preventDefault();
 
-  if (currentPhoneNavState !== "true") {
-    const delta = Math.sign(event.deltaY);
-    const scrollAmount = 500;
+//   if (currentPhoneNavState !== "true") {
+//     const delta = Math.sign(event.deltaY);
+//     const scrollAmount = 500;
 
-    container.scrollBy({
-      top: delta * scrollAmount,
-      behavior: "smooth"
-    });
-  }
-}
+//     container.scrollBy({
+//       top: delta * scrollAmount,
+//       behavior: "smooth"
+//     });
+//   }
+// }
 
-function expandPhoneMenu() {
-  const currentPhoneNavState = phoneNav.getAttribute("expanded");
+// function expandPhoneMenu() {
+//   const currentPhoneNavState = phoneNav.getAttribute("expanded");
 
-  if (currentPhoneNavState === "true") {
-    phoneNav.setAttribute("expanded", "false");
-  }
-  else if (currentPhoneNavState === "false") {
-    phoneNav.setAttribute("expanded", "true");
-  }
-}
+//   if (currentPhoneNavState === "true") {
+//     phoneNav.setAttribute("expanded", "false");
+//   }
+//   else if (currentPhoneNavState === "false") {
+//     phoneNav.setAttribute("expanded", "true");
+//   }
+// }
 
 
 
@@ -88,41 +89,41 @@ function scrollToSection(sectionName){
 // });
 
 
-if (phoneNavComputedStyle.getPropertyValue('display') === 'none'){
-  container.addEventListener('wheel', customScroll);
-  container.setAttribute("phoneScreen", "false");
-}
-else if (navComputedStyle.getPropertyValue('display') === 'none'){
-  container.removeEventListener('wheel', customScroll);
-  container.setAttribute("phoneScreen", "true");
-}
+// if (phoneNavComputedStyle.getPropertyValue('display') === 'none'){
+//   // container.addEventListener('wheel', customScroll);
+//   container.setAttribute("phoneScreen", "false");
+// }
+// else if (navComputedStyle.getPropertyValue('display') === 'none'){
+//   // container.removeEventListener('wheel', customScroll);
+//   container.setAttribute("phoneScreen", "true");
+// }
 
 
-container.addEventListener('keydown', (event) => {
-  const scrollAmount = 0;
+// container.addEventListener('keydown', (event) => {
+//   const scrollAmount = 0;
 
-  if (event.key == "ArrowUp") {
-    scrollAmount = 500;
-  }
-  else if (event.key == "ArrowDown") {
-    scrollAmount = -500;
-  };
+//   if (event.key == "ArrowUp") {
+//     scrollAmount = 500;
+//   }
+//   else if (event.key == "ArrowDown") {
+//     scrollAmount = -500;
+//   };
 
-  container.scrollBy({
-    top: scrollAmount,
-    behavior: "smooth"
-  });
-});
+//   container.scrollBy({
+//     top: scrollAmount,
+//     behavior: "smooth"
+//   });
+// });
 
-container.addEventListener('scroll', function(){
-  const homepageSection = document.querySelector('[is-homepage="true"]');
-  const isHomepage = homepageSection.getBoundingClientRect().top === 0;
+// container.addEventListener('scroll', function(){
+//   const homepageSection = document.querySelector('[is-homepage="true"]');
+//   const isHomepage = homepageSection.getBoundingClientRect().top === 0;
 
-  if (isHomepage) {
-    navBar.setAttribute("homepage-nav", "true");
-  } else {
-    navBar.setAttribute("homepage-nav", "false");
-  }
-  navBar.removeAttribute("initial-load");
-});
+//   if (isHomepage) {
+//     navBar.setAttribute("homepage-nav", "true");
+//   } else {
+//     navBar.setAttribute("homepage-nav", "false");
+//   }
+//   navBar.removeAttribute("initial-load");
+// });
 
